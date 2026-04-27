18e2ac10892ec04a676753dc6e4cf509e2ae44f7
abodaapp-hash <aboda.app@gmail.com>
2026-04-27 06:40:37 +0300

    Update fetch_sheet.py

--- fetch_sheet.py
+++ fetch_sheet.py
@@ -1,33 +1,20 @@
 import requests
 import os
-from datetime import datetime
 import time
 
-# رابط Google Sheets (CSV)
 URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRcHQh5q4mx9jG0FcE1JF1wQo3_luOpO1wWgE_R_iQztu3jziT7juyIWaFWpje2_QwbXnXThCl9Mv--/pub?output=csv"
-
-# إنشاء مجلد data
 os.makedirs("data", exist_ok=True)
 
-# كسر الكاش بإضافة وقت
+# كسر الكاش
 url_with_time = URL + "&t=" + str(int(time.time()))
 
-# تحميل البيانات
 response = requests.get(url_with_time)
 response.encoding = 'utf-8-sig'
 
-# إضافة توقيت داخل الملف (يضمن التحديث)
-content = response.text + f"\n# updated: {datetime.now()}"
-
-# حفظ latest.csv
+# حفظ الملف الأساسي فقط
 with open("data/latest.csv", "w", encoding="utf-8-sig") as f:
-    f.write(content)
+    f.write(response.text)
+    # إضافة سطر في النهاية للتأكد من وجود تغيير لعمل الـ Commit
+    f.write(f"\n# Last Update: {time.ctime()}")
 
-# حفظ نسخة بتاريخ
-timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-filename = f"data/sheet_{timestamp}.csv"
-
-with open(filename, "w", encoding="utf-8-sig") as f:
-    f.write(content)
-
-print("✅ تم التحديث بنجاح")
+print("✅ Updated latest.csv")
